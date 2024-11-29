d3b677e2922495d45bc4e004c1cba9f905085222
prasad <prasadneroolkar90@gmail.com>
2024-11-29 12:08:28 +0530

    css in home in progerss

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,9 +3,31 @@ import React from "react";
 const Home = () => {
   return (
     <>
-      <section className="main">
-        <article>article</article>
+      <section className="main d-flex">
+        {/* <div className="container-fluid">
+          <div className="row">
+            <div className="col-lg-3"> */}
+        <article className="sidebar">
+          <h1>watchlist</h1>
+          <input type="text" />
+          <div className="menu_list">
+            <ul>
+              <li>Home</li>
+              <li>History</li>
+            </ul>
+          </div>
+          <button className="btn_create">create watchlist</button>
+          <hr />
+          <div className="watchlist">
+            <p>My lists</p>
+          </div>
+        </article>
+        {/* </div> */}
+        {/* <div className="col-lg-9"> */}
         <div className="right_common">comman</div>
+        {/* </div> */}
+        {/* </div>
+        </div> */}
       </section>
     </>
   );
