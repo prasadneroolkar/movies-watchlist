27f0401acd6f2fde601d96c6c46bb76761ce094f
prasad <prasadneroolkar90@gmail.com>
2024-11-30 12:09:21 +0530

    sidebar in progress

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -1,12 +1,36 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import Home from "/images/Home.png";
+import history from "/images/history.png";
 
 const Menu = () => {
+  const menuList = [
+    {
+      name: "Home",
+      path: "",
+      icon: Home,
+    },
+    {
+      name: "History",
+      path: "",
+      icon: history,
+    },
+  ];
   return (
     <>
       <div className="menu_list">
         <ul>
-          <li>Home</li>
-          <li>History</li>
+          {menuList.map((val, index) => (
+            <li
+              className="d-flex justify-content-start align-items-center"
+              key={index}
+            >
+              <Link to={val.path}>
+                <img src={val.icon} alt={`${val.name} Icon`} />
+                {val.name}
+              </Link>
+            </li>
+          ))}
         </ul>
       </div>
     </>
