a21bd68a9ba0c8d8a8eb7c3ea4e34e59364d376b
prasad <prasadneroolkar90@gmail.com>
2024-12-11 12:05:06 +0530

    working on home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,32 @@
 import React from "react";
 import Siderbar from "../components/Siderbar";
+import ribbon from "/images/ribbon.png";
+import tick from "/images/tick.png";
+import Searchbar from "../components/Searchbar";
+import Button from "../components/Button";
 
 const Home = () => {
-  return <>home</>;
+  return (
+    <>
+      <section className="home-message">
+        <h1>
+          Welcome to <span>Watchlists</span>
+        </h1>
+        <p>
+          Browse movies, add them to watchlists and share them with friends.
+        </p>
+        <p>
+          Just click the
+          <img src={ribbon} /> to add a movie, the poster to see more details or{" "}
+          <img src={tick} /> to mark the movie as watched.
+        </p>
+      </section>
+      <section>
+        <Searchbar />
+        <Button btnName="search"></Button>
+      </section>
+    </>
+  );
 };
 
 export default Home;
