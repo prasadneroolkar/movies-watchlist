83421c7554b9905d7c16ba436a1f940a6ac74451
prasad <prasadneroolkar90@gmail.com>
2024-12-13 21:06:56 +0530

    fetching api movie in home component

--- src/main.jsx
+++ src/main.jsx
@@ -5,11 +5,11 @@ import { BrowserRouter } from "react-router-dom";
 import AuthProvider from "./context/AuthContext.jsx";
 
 createRoot(document.getElementById("root")).render(
-  <StrictMode>
-    <BrowserRouter>
-      <AuthProvider>
-        <App />
-      </AuthProvider>
-    </BrowserRouter>
-  </StrictMode>
+  // <StrictMode>
+  <BrowserRouter>
+    <AuthProvider>
+      <App />
+    </AuthProvider>
+  </BrowserRouter>
+  // </StrictMode>
 );
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -7,13 +7,42 @@ import { api } from "../services/api";
 
 const Home = () => {
   const [movies, setMovies] = useState([]);
+  const [prev, setPrev] = useState(0);
+  const [next, setNext] = useState(5);
+
   useEffect(() => {
     const fetchApi = async () => {
+      console.log("useeffect called");
       const fetchMovies = await api();
       setMovies(fetchMovies);
     };
     fetchApi();
   }, []);
+
+  // console.log("movies array", movies);
+  const newMovies = movies.slice(prev, next);
+  // console.log(newMovies);
+
+  const onPrev = () => {
+    console.log("prev buttn");
+    if (prev !== 0 || prev < -1) {
+      setPrev((prev) => prev - 5);
+      setNext((next) => next - 5);
+      // console.log(prev);
+      // console.log(next);
+    }
+  };
+
+  const onNext = () => {
+    console.log("next buttn");
+
+    if (next < movies.length) {
+      setNext((next) => next + 5);
+      setPrev((prev) => prev + 5);
+      // console.log(next);
+      // console.log(prev);
+    }
+  };
 
   return (
     <>
@@ -36,11 +65,18 @@ const Home = () => {
       </section>
       <section className="movies_section">
         <h2>Popular movies right now</h2>
-        <div className="d-flex column-gap-2">
+        <div className="d-flex card-main">
+          <button
+            className="carousel-btn prev"
+            onClick={onPrev}
+            disabled={prev === 0}
+          >
+            &#8249;
+          </button>
           {movies.length === 0 ? (
             <p>No movies found.</p>
           ) : (
-            movies.map((movie) => (
+            newMovies.map((movie) => (
               <div className="mov_card" key={movie.imdbID}>
                 <span>
                   <img src="/images/ribbon2.png" alt="ribbon" />
@@ -58,6 +94,13 @@ const Home = () => {
               </div>
             ))
           )}
+          <button
+            className="carousel-btn next"
+            onClick={onNext}
+            disabled={next >= movies.length}
+          >
+            &#8250;
+          </button>
         </div>
       </section>
     </>
