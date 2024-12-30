ba1d03baa1c7b5193f0d5012f0b6f70180c6be18
prasad <prasadneroolkar90@gmail.com>
2024-12-30 18:16:05 +0530

    styling is inl progres for watchlist page

--- src/pages/WatchlistPage.jsx
+++ src/pages/WatchlistPage.jsx
@@ -1,14 +1,20 @@
 import React from "react";
 import PageTitle from "../components/watchlist/PageTitle";
+import editBtn from "/images/editBtn.png";
 
 const WatchlistPage = () => {
   return (
-    <section>
-      <PageTitle
-        className="create_watchlist mb-0"
-        Title="Movies by Tom Cruise"
-      />
-      <img src="" alt="" />
+    <section className="watchlist_page">
+      <div className="editbtn">
+        <PageTitle className="mb-0" Title="Movies by Tom Cruise" />
+        <img src={editBtn} alt="" />
+      </div>
+      <div className="description">
+        <h3>About this watchlist</h3>
+        <p>This list lorem ipsum dolor et blah blah blah</p>
+      </div>
+
+      <div className="movie_list"></div>
     </section>
   );
 };
