a7aabd1a4db481f79469f73c94abdd7623edd79a
prasad <prasadneroolkar90@gmail.com>
2025-02-27 21:16:11 +0530

    working on localstorage retriavl

--- src/components/store/localWatchlistSlice.js
+++ src/components/store/localWatchlistSlice.js
@@ -19,27 +19,15 @@ const localwatchlistSlice = createSlice({
         JSON.parse(localStorage.getItem("watchlists")) || [];
       state.length = 0; // Clear the existing state
       state.push(...storedWatchlist); // Populate it with the new data
-      console.log("storaed locallist", storedWatchlist);
     },
 
     updatelocalliststorage(state, action) {
       const { localListid, localMovie } = action.payload;
 
       const localList = state.find((lw) => lw.id === localListid);
-      // console.log("localID store", localListid);
-
-      // console.log("localList store", localList);
-      // console.log(
-      //   "LocalWatchlist found:",
-      //   JSON.parse(localStorage.watchlists, null, 2)
-      // );
 
       if (localList) {
-        // console.log(
-        //   "if Localmovie exist in movie",
-        //   // JSON.parse(localStorage.movi, null, 2)
-        //   localMovie
-        // );
+        console.log("localList movies", localList.movies);
 
         if (!Array.isArray(localList.movies)) {
           localList.movies = [];
@@ -51,10 +39,7 @@ const localwatchlistSlice = createSlice({
 
         if (!movieExists) {
           localList.movies.push(localMovie);
-          // console.log(
-          //   "Movie added to watchlist:",
-          //   JSON.stringify(localList, null, 2)
-          // );
+          localStorage.setItem("watchlists", JSON.stringify(state));
         } else {
           console.log("Movie already exists in the Localwatchlist");
         }
